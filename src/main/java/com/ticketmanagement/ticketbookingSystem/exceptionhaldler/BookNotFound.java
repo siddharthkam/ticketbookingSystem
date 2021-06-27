a0f3bfbdbb46d1f032ac55eee4342a7cc37d1570
LAPTOP-3RLF7SP0\SIDD <siddharthkamble1992@gmail.com>
2021-06-27 16:56:11 +0530

    adding constrcutor

--- src/main/java/com/ticketmanagement/ticketbookingSystem/exceptionhaldler/BookNotFound.java
+++ src/main/java/com/ticketmanagement/ticketbookingSystem/exceptionhaldler/BookNotFound.java
@@ -2,4 +2,14 @@ package com.ticketmanagement.ticketbookingSystem.exceptionhaldler;
 
 public class BookNotFound extends Exception {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public BookNotFound()
+	{
+		super("Book Not found");
+	}
+
 }
