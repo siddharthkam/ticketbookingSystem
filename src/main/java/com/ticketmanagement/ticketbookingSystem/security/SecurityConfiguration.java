d707326f44e8f1afd644567650b366f14ff96190
LAPTOP-3RLF7SP0\SIDD <siddharthkamble1992@gmail.com>
2021-07-18 23:55:12 +0530

    added same line2

--- src/main/java/com/ticketmanagement/ticketbookingSystem/security/SecurityConfiguration.java
+++ src/main/java/com/ticketmanagement/ticketbookingSystem/security/SecurityConfiguration.java
@@ -34,6 +34,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		auth.inMemoryAuthentication().withUser("SIDDHARTH5").password("{noop}PASSWORD").roles("USER","ADMIN");
 		auth.inMemoryAuthentication().withUser("SIDDHARTH6").password("{noop}PASSWORD").roles("USER","ADMIN");
 		auth.inMemoryAuthentication().withUser("SIDDHARTH7").password("{noop}PASSWORD").roles("USER","ADMIN");
+		auth.inMemoryAuthentication().withUser("SIDDHARTH8").password("{noop}PASSWORD").roles("USER","ADMIN");
 		
 	}
 
