727404a927bef71a69d230b84465534ab79fae7c
LAPTOP-3RLF7SP0\SIDD <siddharthkamble1992@gmail.com>
2021-07-18 23:18:25 +0530

    Commit Changes

--- src/main/java/com/ticketmanagement/ticketbookingSystem/exceptionhaldler/BusinessDateValidator.java
+++ src/main/java/com/ticketmanagement/ticketbookingSystem/exceptionhaldler/BusinessDateValidator.java
@@ -1,5 +1,12 @@
 package com.ticketmanagement.ticketbookingSystem.exceptionhaldler;
 
 public class BusinessDateValidator extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public BusinessDateValidator()
+	{
+		super("Invalid Date");
+	}
 
 }
