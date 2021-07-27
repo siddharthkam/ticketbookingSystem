bd6cab549533f6a8552296d8be6a77ad28107d08
LAPTOP-3RLF7SP0\SIDD <siddharthkamble1992@gmail.com>
2021-07-27 21:21:29 +0530

    Actuator Changes

---
+++ src/main/java/com/ticketmanagement/ticketbookingSystem/customendspoint/CustomEndPoint.java
@@ -0,0 +1,40 @@
+package com.ticketmanagement.ticketbookingSystem.customendspoint;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.annotation.Selector;
+import org.springframework.stereotype.Component;
+
+@Endpoint(id="release-note")
+@Component
+public class CustomEndPoint {
+	
+	Map<String, List<String>> releaseNoteMap=new LinkedHashMap<>();
+	
+	@PostConstruct
+	public void initNotes()
+	{
+		releaseNoteMap.put("Version-1.0",Arrays.asList("Release First One","Release SECOND One"));
+		releaseNoteMap.put("Version-1.1",Arrays.asList("Release THIRD One","Release FOUR One"));
+	}
+	
+	@ReadOperation
+	public Map<String, List<String>> getReleaseNote()
+	{
+		return releaseNoteMap;
+	}
+	
+	@ReadOperation
+	public List<String> getReleaseNoteBySelector(@Selector String version)
+	{
+		return releaseNoteMap.get(version);
+	}
+
+}
